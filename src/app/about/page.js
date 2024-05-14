e185402a14d56dd4b084d6b10d6283e875e0f15c
ST-Ko <sithu.ko21120@gmail.com>
2024-05-14 14:56:48 +1000

    add about page

---
+++ src/app/about/page.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>Welcome to our movie database</p>
+      <p>
+        Our website is designed to provide you with a comprehensive database of
+        movies from all around the world.
+      </p>
+      <p>
+        In addition to our extensive movie database, we also discuss your
+        favourite films with like-minded individuals.
+      </p>
+    </div>
+  );
+};
+
+export default page;
