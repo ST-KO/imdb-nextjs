f69a1ace82ded81af6f9deb4a526b2a9ef0058f8
ST-Ko <sithu.ko21120@gmail.com>
2024-05-15 16:04:22 +1000

    add search box

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -6,7 +6,7 @@ const page = async ({params}) => {
 
     const res = await fetch (`https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`)
     const movie = await res.json();
-    console.log(movie);
+
     return (
     <div className='w-full'>
         <div className='p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6xl mx-auto md:space-x-6'>
